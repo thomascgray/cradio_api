f1ec9612aef2029dd2ab81021d13b491410df410
Unknown <tom.c.gray@gmail.com>
2017-07-12 08:44:13 +0100

    boiler plate

--- rooms/model.js
+++ rooms/model.js
@@ -5,36 +5,78 @@ let rooms = [];
 const roomStatuses = {
     "PLAYING" : 1,
     "PAUSED" : 2,
+    "LOADING" : 3,
+    "FINISHED" : 4,
 }
 
+/**
+ * create a new, generated room object and return it
+ * - ID: a random uuid()
+ * - status: defaults to roomStatuses.PAUSED
+ * - songs: empty array
+ *
+ * @return {object} a generated room
+ */
 model.generate = () => {
     return {
         id: uuid(),
         status: roomStatuses.PAUSED,
+        songs: [],
     };
 }
 
+/**
+ * return all the rooms that currently exist
+ * @return {array}
+ */
 model.getAllRooms = () => {
     return rooms;
 }
 
+/**
+ * add a room to the pool
+ * @param {room} r the room to add to the pool
+ * @return {this}
+ */
 model.addRoom = (r) => {
+    if (r.id == null) {
+        return false;
+    }
     rooms.push(r);
+    return this;
 }
 
+/**
+ * return a room by its id
+ * @param  {string} id
+ * @return {object} the room
+ */
 model.getRoom = id => {
     return rooms.find(r => {
         return id === r.id;
     });
 }
 
+/**
+ * based on an id, remove a room from the room pool
+ * @param  {string} id
+ * @return {this}
+ */
 model.deleteRoom = id => {
     let rIndex = rooms.findIndex(r => {
         return id === r.id;
     });
     rooms.splice(rIndex, 1);
+    return this;
 }
 
+/**
+ * given a room id and a status, set that room's status
+ * as the given status
+ * @param {string} id
+ * @param {int} status
+ * @return {this}
+ */
 model.setRoomStatus = (id, status) => {
     rooms = rooms.map(r => {
         if (r.id === id) {
@@ -42,6 +84,26 @@ model.setRoomStatus = (id, status) => {
         }
         return r;
     });
+
+    return this;
+};
+
+/**
+ * given a room id and a song, add that song
+ * to the rooms songs array
+ * @param {string} id
+ * @param {object} song
+ * @return {this}
+ */
+model.addSong = (id, song) => {
+    rooms = rooms.map(r => {
+        if (r.id === id) {
+            r.songs.push(song);
+        }
+        return r;
+    });
+
+    return this;
 };
 
 model.roomStatuses = roomStatuses;
--- rooms/routes.js
+++ rooms/routes.js
@@ -2,45 +2,50 @@ const express = require('express');
 const roomModel = require('./model');
 const router = express.Router();
 
+// return all the rooms
 router.get('/', (req, res, next) => {
     res.send(roomModel.getAllRooms());
 });
 
+// generate a new room, add it to the room pool and return it
 router.get('/generate', (req, res, next) => {
-    roomModel.addRoom(roomModel.generate());
-    res.status(201).send();
+    let tempRoom = roomModel.generate();
+    roomModel.addRoom(tempRoom);
+    res.status(201).send(tempRoom);
 });
 
+// get a room
 router.get('/:id', (req, res, next) => {
     res.send(roomModel.getRoom(req.params.id));
 });
 
+// delete a room
 router.delete('/:id', (req, res, next) => {
     roomModel.deleteRoom(req.params.id);
     res.status(200).send();
 });
 
-router.get('/:id/play', (req, res, next) => {
+// attempt to set a room as playing
+router.get('/:id/action/play', (req, res, next) => {
     roomModel.setRoomStatus(req.params.id, roomModel.roomStatuses.PLAYING);
     res.status(200).send();
 });
 
-router.get('/:id/pause', (req, res, next) => {
+// attempt to set a room as paused
+router.get('/:id/action/pause', (req, res, next) => {
     roomModel.setRoomStatus(req.params.id, roomModel.roomStatuses.PAUSED);
     res.status(200).send();
 });
 
-router.get('/:id/add-song', (req, res, next) => {
-    roomModel.addSong(req.params.id, roomModel.roomStatuses.PAUSED);
+router.get('/:id/report/song-finished', (req, res, next) => {
+    roomModel.setRoomStatus(req.params.id, roomModel.roomStatuses.PAUSED);
     res.status(200).send();
 });
 
 
-
-
-
-
-
-
+router.get('/:id/action/add-song', (req, res, next) => {
+    roomModel.addSong(req.params.id, roomModel.roomStatuses.PAUSED);
+    res.status(200).send();
+});
 
 module.exports = router;
